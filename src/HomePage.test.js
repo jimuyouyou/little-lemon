b164f84949817f0a2c04139c591c4c209fc008ec
jimuyouyou <qinfuxian2014@outlook.com>
2023-07-26 19:49:45 +0800

    fix test issue

--- src/HomePage.test.js
+++ src/HomePage.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import HomePage from "./components/HomePage";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<HomePage />);
+  const linkElement = screen.getByText(/traditional recipes/i);
   expect(linkElement).toBeInTheDocument();
 });
